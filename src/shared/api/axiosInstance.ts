668e4e1d20aaa6040b5b9bb6ceb8a0c6503e9e28
Tnalxmsk <alstnwkd990@naver.com>
2025-08-10 04:13:11 +0900

    :sparkles: feat: 토큰 관리 로직 구현

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "@/src/shared/store/auth";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -6,4 +7,39 @@ export const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-})
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const { accessToken } = useAuthStore.getState();
+  if (!accessToken) return config;
+  (config.headers as Record<string, any>).Authorization = `Bearer ${accessToken}`;
+  return config;
+});
+
+const MAX_RETRY_COUNT = 2;
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (originalRequest.url?.includes('/auth/refresh-token')) {
+      return Promise.reject(error);
+    }
+
+    if (originalRequest._retryCount === undefined) {
+      originalRequest._retryCount = 0;
+    }
+
+    if ((error.response.status === 401 || error.response.status === 403) && originalRequest._retryCount < MAX_RETRY_COUNT) {
+      originalRequest._retryCount++;
+      try {
+        await axiosInstance.post("/auth/refresh-token");
+        return axiosInstance(originalRequest);
+      } catch ( refreshError ) {
+        useAuthStore.getState().logout();
+      }
+    }
+    throw error;
+  },
+);
