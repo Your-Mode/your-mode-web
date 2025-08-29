a6505ea9922f8369fa0e17b8b7d36d09439e7f78
Tnalxmsk <alstnwkd990@naver.com>
2025-08-30 01:25:22 +0900

    :sparkles: feat: 회원정보 수정 및 비밀번호 변경 기능 구현

--- src/shared/api/user.ts
+++ src/shared/api/user.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@/src/shared/api/axiosInstance";
 import { MyProfileResponse } from "@/src/shared/types/user";
+import axios from "axios";
 
 export const getMyProfile = async () => {
   try {
@@ -8,5 +9,42 @@ export const getMyProfile = async () => {
     return response.data;
   } catch ( error ) {
     console.error(error);
+  }
+};
+
+export interface UpdateMyProfileRequest {
+  name: string;
+  phoneNumber: string;
+  height: number;
+  weight: number;
+  gender: string;
+  bodyTypeId: number;
+}
+
+export const updateMyProfile = async (data: UpdateMyProfileRequest) => {
+  try {
+    const response = await axiosInstance.put('/users/me', data);
+    return response.data;
+  } catch ( error ) {
+    console.error(error);
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    }
+  }
+};
+
+export interface UpdatePasswordRequest {
+  newPassword: string;
+}
+
+export const updatePassword = async (request: UpdatePasswordRequest) => {
+  try {
+    const response = await axiosInstance.post<BaseResponse<string>>('/users/me/password', request);
+    return response.data;
+  } catch ( error ) {
+    console.error(error);
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    }
   }
 };
