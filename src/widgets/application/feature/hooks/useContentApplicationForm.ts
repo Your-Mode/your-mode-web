1f5e67ae398c2d1c9ec2231fbb21151fafb5d74f
Tnalxmsk <alstnwkd990@naver.com>
2025-08-30 01:39:42 +0900

    :recycle: refactor: 수정 폼 수정

--- src/widgets/application/feature/hooks/useContentApplicationForm.ts
+++ src/widgets/application/feature/hooks/useContentApplicationForm.ts
@@ -3,10 +3,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
 const formSchema = z.object({
-  name: z.string().min(1, "이름을 입력해주세요."),
-  bodyType: z.string().min(1, "체형을 입력해주세요."),
-  height: z.string().min(2, "키를 입력해주세요."),
-  weight: z.string().min(2, "몸무게를 입력해주세요."),
   bodyFeatures: z.string().min(20, "20자 이상 입력").max(200, "200자 이하"),
   recommendedItems: z.array(z.string()).min(1, "최소 1개 이상 선택"),
   situation: z.string().min(20, "20자 이상 입력").max(200, "200자 이하"),
@@ -22,8 +18,6 @@ export const useContentApplicationForm = () => {
   return useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "김정윤",
-      bodyType: "웨이브 체형",
       recommendedItems: [],
       uploadConsent: false,
     },
